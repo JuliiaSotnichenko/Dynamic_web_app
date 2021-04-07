e56ad82fcb8580bf8e66bb95f938bf96f56588cc
Juliia Sotnichenko <juliia.sotnichenko@gmail.com>
2021-04-07 10:21:04 +0200

    database

--- register.php
+++ register.php
@@ -1,8 +1,42 @@
 <?php
 
 
+$errors = array();
+
+if (isset($_POST['btnReg'])) {
+    $firstName = trim($_POST['first_name']);
+    $lastName = trim($_POST['last_name']);
+    $mail = trim($_POST['email']);
+    $password = trim($_POST['password']);
+    $sanitizeEmail = filter_var($mail, FILTER_SANITIZE_EMAIL);
+
+if (empty($firstName)) {
+    $errors['first_name'] = 'First name is mandatory.<br>';
+}
+
+if (empty($lastName)) {
+    $errors['last_name'] = 'Last name is mandatory.<br>';
+}
+
+if (!filter_var($sanitizeEmail, FILTER_VALIDATE_EMAIL)) {
+    $errors['email'] = 'Email has to be a valid one.<br>';
+}
+
+if (empty($password)) {
+    $errors['password'] = 'Password is mandatory.<br>';
+}
 
 
+if (count($errors)) == 0) {
+    
+    include_once 'database.php';
+    $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+
+
+}
+
+
+}
 
 
 
@@ -36,13 +70,19 @@
 
 
 <form action="" method="POST">
-        <input type="email" name="email" placeholder="Email"><br>
+        <input type="email" name="email" placeholder="Enter your email"><br>
+        <?php if (isset($errors['email'])) echo $errors['email'] ?>
 
-        <input type="password" name="password" placeholder="Password"><br>
-        <input type="first_name" name="first_name" placeholder="Enter your First name"><br>
-        <input type="last_name" name="last_name" placeholder="Enter your Last name"><br>
+        <input type="password" name="password" placeholder="Enter your password"><br>
+        <?php if (isset($errors['password'])) echo $errors['password'] ?>
 
-        <input type="submit" name="login" value="Register">
+        <input type="text" name="first_name" placeholder="Enter your First name"><br>
+        <?php if (isset($errors['first_name'])) echo $errors['first_name'] ?>
+
+        <input type="text" name="last_name" placeholder="Enter your Last name"><br>
+        <?php if (isset($errors['last_name'])) echo $errors['last_name'] ?>
+
+        <input type="submit" name="btnReg" value="Register">
     </form>
 
 
